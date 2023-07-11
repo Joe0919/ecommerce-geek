be16144c183647263655b4541a5f39981b3fd3aa
Joel0919 <joel09llalli@gmail.com>
2023-07-10 20:10:20 -0500

    Add action for send message to email

---
+++ assets/js/toggle.js
@@ -0,0 +1,24 @@
+// $(window, document, undefined).ready(function () {
+//   $(".contacto__form-input").blur(function () {
+//     var $this = $(this);
+//     if ($this.val()) $this.addClass("used");
+//     else $this.removeClass("used");
+//   });
+// });
+
+window.addEventListener("load", () => {
+  const inputs = document.querySelectorAll(".contacto__form-input");
+
+  inputs.forEach(function (input) {
+    console.log(input);
+
+    input.addEventListener("blur", () => {
+        if (input.value) {
+            input.classList.add("used")
+        } else {
+            input.classList.remove("used")  
+        }
+    });
+
+  });
+});
